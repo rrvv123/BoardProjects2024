219784cad0b7ff6e64fca846edbb74f30deca235
rrvv123 <eorms7391@gmail.com>
2024-08-27 16:03:10 +0900

    0827 - 8교시 Add Reply (Entity)

---
+++ src/main/java/kr/ac/kopo/board/entity/Reply.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor //
+@NoArgsConstructor // 기본생성자
+@Getter
+@ToString
+public class Reply extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    private String text;
+
+    private String replyer;
+}
